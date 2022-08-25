947c1873510da5f52da194218a477e975782a60a
Simon Golms <development@gol.ms>
2022-08-25 11:05:44 +0200

    feat(hooks): add options to update search-params
    - use push option as default

--- src/utils/useSearchParams.ts
+++ src/utils/useSearchParams.ts
@@ -1,8 +1,22 @@
-import { useMemo } from 'react';
-import { useLocation } from 'react-router';
+import { useCallback, useMemo } from 'react';
+import { useHistory, useLocation } from 'react-router';
 
 export const useSearchParams = () => {
   const { search } = useLocation();
+  const history = useHistory();
 
-  return useMemo(() => new URLSearchParams(search), [search]);
+  const searchParams = useMemo(() => Object.fromEntries(new URLSearchParams(search)), [search]);
+  const setSearchParams = useCallback(
+    (newSearchParams: Record<string, string>, method: 'push' | 'replace' = 'push') => {
+      if (method === 'push') {
+        history.push({ search: new URLSearchParams(newSearchParams).toString() });
+      }
+      if (method === 'replace') {
+        history.replace({ search: new URLSearchParams(newSearchParams).toString() });
+      }
+    },
+    [history]
+  );
+
+  return { searchParams, setSearchParams };
 };
