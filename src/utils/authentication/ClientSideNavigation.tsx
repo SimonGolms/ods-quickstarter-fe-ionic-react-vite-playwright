e91a2987c304c2390ce84e371b1b07c1e28e9eaa
Golms,Simon (IT EDP) BIG-DE-B <simon.golms@boehringer-ingelheim.com>
2023-02-22 09:40:07 +0100

    docs: add url to code example

--- src/utils/authentication/ClientSideNavigation.tsx
+++ src/utils/authentication/ClientSideNavigation.tsx
@@ -3,7 +3,10 @@ import { PublicClientApplication } from "@azure/msal-browser";
 import { useHistory } from "react-router-dom";
 import { CustomNavigationClient } from "./NavigationClient";
 
-// This component is needed to take advantage of the router's navigate functions when MSAL redirects between pages in your app
+/**
+ * This component is optional. This is how you configure MSAL to take advantage of the router's navigate functions when MSAL redirects between pages in your app
+ * @see: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/09f65ee73fa849e71e3a73b857f562fab9d47263/samples/msal-react-samples/react-18-sample/src/App.js#L38-L54
+ */
 export const ClientSideNavigation = ({ children, instance }: TProps) => {
   const navigate = useHistory();
   const navigationClient = new CustomNavigationClient(navigate);
